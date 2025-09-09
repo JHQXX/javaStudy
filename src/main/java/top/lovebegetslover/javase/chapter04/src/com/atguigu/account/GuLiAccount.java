773ab8488151bd882566a08faa91f9d06d9923df
李志 <2733827845@qq.com>
2025-09-09 11:23:17 +0800

    feat : 尚硅谷-Java-数组

---
+++ src/main/java/top/lovebegetslover/javase/chapter04/src/com/atguigu/account/GuLiAccount.java
@@ -0,0 +1,12 @@
+package top.lovebegetslover.javase.chapter04.src.com.atguigu.account;
+
+/**
+ * ClassName: GuLiAccount
+ * Description:
+ *
+ * @Author 尚硅谷-宋红康
+ * @Create 2022/11/15 15:46
+ * @Version 1.0
+ */
+public class GuLiAccount {
+}
---
+++ src/main/java/top/lovebegetslover/javase/chapter05_array/src/com/atguigu1/one/exer2/ArrayExerEnglishTranslation.java
@@ -0,0 +1,13 @@
+package top.lovebegetslover.javase.chapter05_array.src.com.atguigu1.one.exer2;
+
+/**
+ * @Author: Lee
+ * @Description: TODO
+ * @DateTime: 2025/9/9 上午11:22
+ **/
+public class ArrayExerEnglishTranslation {
+    public static void main(String[] args) {
+        //创建数组来存储周一到周日的单词
+
+    }
+}
