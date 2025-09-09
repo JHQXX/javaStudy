9f9cc839d22c2cf2fce20d8dde710e28483759db
李志 <2733827845@qq.com>
2025-09-09 14:19:39 +0800

    feat : 尚硅谷-Java-数组

--- src/main/java/top/lovebegetslover/javase/chapter05_array/src/com/atguigu1/one/exer2/ArrayExerEnglishTranslation.java
+++ src/main/java/top/lovebegetslover/javase/chapter05_array/src/com/atguigu1/one/exer2/ArrayExerEnglishTranslation.java
@@ -1,5 +1,7 @@
 package top.lovebegetslover.javase.chapter05_array.src.com.atguigu1.one.exer2;
 
+import java.util.Scanner;
+
 /**
  * @Author: Lee
  * @Description: TODO
@@ -8,6 +10,9 @@ package top.lovebegetslover.javase.chapter05_array.src.com.atguigu1.one.exer2;
 public class ArrayExerEnglishTranslation {
     public static void main(String[] args) {
         //创建数组来存储周一到周日的单词
+        String[] weeks={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入数组");
 
     }
 }
