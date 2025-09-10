dcf996c6e22260dbcfb0736f7d400ce9cf0335d0
lizhi <2733827845@qq.om>
2025-09-10 12:40:38 +0800

    feat : 尚硅谷-Java-数组

---
+++ src/main/java/top/lovebegetslover/javase/chapter05_array/src/com/atguigu1/one/exer3/ArrayExerStudentLeve.java
@@ -0,0 +1,14 @@
+package top.lovebegetslover.javase.chapter05_array.src.com.atguigu1.one.exer3;
+
+import java.util.Scanner;
+
+public class ArrayExerStudentLeve {
+    public static void main(String[] args) {
+        //创建对应的等级
+        String[] leve={"A","B","C","D"};
+        //系统读取
+        Scanner scanner = new Scanner(System.in);
+        //拿去读取的数据--学生人数
+
+    }
+}
