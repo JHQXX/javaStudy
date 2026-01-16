5e3019bd49e07dfb9fc3b0bd14d20676443060d0
Lee <2733827845@qq.com>
2026-01-16 16:49:00 +0800

    feat : javaSe项目提交

--- JavaCore/src/main/java/cn/jhqxx/concurrency/test.java
+++ JavaCore/src/main/java/cn/jhqxx/concurrency/test.java
@@ -1,5 +1,9 @@
 package cn.jhqxx.concurrency;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @author jhqxx 梦想成为工程师的人
  * @date 2026/1/7 22:01
@@ -7,10 +11,6 @@ package cn.jhqxx.concurrency;
  */
 public class test {
     public static void main(String[] args) {
-        String key = "";
-        for (int i = 0; i < 10; i++) {
-            int random  = (int) (Math.random() * (10 + 1));
-            System.out.println(random);
-        }
+       return;
     }
 }
